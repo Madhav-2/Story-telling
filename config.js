0c58a1b4e1aa085411946e2db50e0dcabab83b32
Madhav-2 <72733842+Madhav-2@users.noreply.github.com>
2021-06-22 18:53:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+  export const firebaseConfig = {
+    apiKey: "AIzaSyCCX40rbTthSyOF_ynxLZ2La1IZvxHQ-g8",
+    authDomain: "story-telling-app-c9274.firebaseapp.com",
+    databaseURL: "https://story-telling-app-c9274-default-rtdb.firebaseio.com",
+    projectId: "story-telling-app-c9274",
+    storageBucket: "story-telling-app-c9274.appspot.com",
+    messagingSenderId: "80118680484",
+    appId: "1:80118680484:web:766a2d3c65f0d086328f31"
+    };
